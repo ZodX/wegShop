740ff04974c1462b53bf8fc2a2df7a3c04ab2f40
Molnár András Imre <m.andras.imre00@gmail.com>
2020-12-19 12:30:15 +0100

    Redirecting user when trying to reach add page

--- Frontend/src/app/components/product/add/add.component.ts
+++ Frontend/src/app/components/product/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.username = sessionStorage.getItem('authenticatedUser');
+
+    if (this.username === 'user')
+      this.router.navigate(['/main']);
   }
 
 }
